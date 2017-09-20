d9a0439ce5ae895b39d2df44b0a0c228181a9fb8
Ivo Kroon <ivokroon@MacBook-Pro-van-ivo.local>
2017-09-20 16:06:13 +0200

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+let HTMLWebpackPlugin = require('html-webpack-plugin');
+let HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+    template: __dirname + '/src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+module.exports = {
+    entry: __dirname + '/src/index.js',
+    module: {
+        loaders:[
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    output:{
+        filename: "transformed.js",
+        path: __dirname + "/public"
+    },
+    plugins: [HTMLWebpackPluginConfig]
+};
